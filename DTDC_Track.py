c33e8c51e79315ed039aaea230d54167ccd36439
Balmohan <balmohan.job@gmail.com>
2017-02-13 22:58:44 +0530

    Turn around time added

--- DTDC_Track.py
+++ DTDC_Track.py
@@ -1,23 +1,31 @@
 import requests as r
-from tkinter import ttk
 from tkinter import filedialog
 from tkinter import *
+from tkinter import messagebox
 import time
+start=time.time()
 class GettrackData:
     track = []
     root = Tk()
     root.withdraw()
-    jdata =[ ]
-    rdict = {'activityType': '', 'dateWithNoSuffix': '', 'deliveryStatus': '', 'origin': '', 'time': '', 'orgCode': '',
-             'mode': ''}
+    jdata = []
+    rdict = {'activityType': '', 'dateWithNoSuffix': '', 'deliveryStatus': '', 'origin': '', 'time': '',
+                 'orgCode': '',
+                 'mode': ''}
     def openfile () :
         path = filedialog.askopenfilename(filetype=(("txt", "*.txt"), ("All Files", "*.*")))
         file = open(path, 'r')
         return file
-    def readfile(self):         #Read Opened File
+    def readfile():         #Read Opened File
         for i in GettrackData.openfile():
             GettrackData.track.append(i.strip("\n"))
         return GettrackData.track
+
+    def SaveFile(self):
+
+            file=open("Tracking_Result.txt",'a')
+            file.write(("\n"+GettrackData.rdict['dateWithNoSuffix']+" "+GettrackData.rdict['time']
+                          +" "+GettrackData.rdict['activityType']+"("+GettrackData.rdict['origin']+") "+GettrackData.rdict['deliveryStatus']))
 
     def getjsondata(track):
             i=track
@@ -34,14 +42,15 @@ class GettrackData:
                             GettrackData.rdict[i]=jin[i]
                             # uncomment this for detailed records
                     """print(GettrackData.rdict['dateWithNoSuffix']+" "+GettrackData.rdict['time']
-                          +" "+GettrackData.rdict['activityType']+" ("+GettrackData.rdict['origin']+") "+GettrackData.rdict['deliveryStatus'])
-                    wfile.write("\n"+GettrackData.rdict['dateWithNoSuffix']+" "+GettrackData.rdict['time']
-                          +" "+GettrackData.rdict['activityType']+"("+GettrackData.rdict['origin']+") "+GettrackData.rdict['deliveryStatus'])"""
+                          +" "+GettrackData.rdict['activityType']+" ("+GettrackData.rdict['origin']+") "+GettrackData.rdict['deliveryStatus'])"""
+
                 wfile.close()
-Getdat=GettrackData()
-rcords=Getdat.readfile()
+
+rcords=GettrackData.readfile()
+
 for i in range(len(rcords)):
     print("\n"+"Record for  " + GettrackData.track[i])
     GettrackData.getjsondata(i)
-    print("Latedt updates are "+GettrackData.rdict['deliveryStatus']+" at "+GettrackData.rdict['origin']+"( "+GettrackData.rdict['activityType']+")")
+    print("Latest updates are "+GettrackData.rdict['deliveryStatus']+" at "+GettrackData.rdict['origin']+"( "+GettrackData.rdict['activityType']+")")
 #print("Your Tracking Details are stored in Tracking_Results.txt\nExiting Windows Please wait")
+print("%s Seconds"%(time.time()-start))
--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Add all your Tracking No. in single text file seperated by new line(each line on
 if your using python to run script you can directly execute as[ python [file_name.py]]<br/>
 
 #DTDC_Mass_Tracking <br/>
-Uncomment print and file.write if you want detailed tracking history
+Uncomment print and file.write if you want detailed tracking history <br/>
+Turn around time 100 valid records in 50 seconds
