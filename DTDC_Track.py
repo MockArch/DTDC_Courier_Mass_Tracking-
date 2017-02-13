ad6cc49047c53d006493e8f3b7dedc9c73bb66d8
Balmohan <balmohan.job@gmail.com>
2017-02-13 16:07:22 +0530

    comments added file added

--- DTDC_Track.py
+++ DTDC_Track.py
@@ -3,17 +3,17 @@ from tkinter import filedialog
 from tkinter import *
 import time
 class GettrackData:
-    track=[]
-    root=Tk()
-    jdata=[]
+    track = []
+    root = Tk()
+    jdata =[ ]
     rdict = {'activityType': '', 'dateWithNoSuffix': '', 'deliveryStatus': '', 'origin': '', 'time': '', 'orgCode': '',
              'mode': ''}
-    def openfile():
-        path=filedialog.askopenfilename(filetype=(("txt","*.txt"),("All Files","*.*")))
-        file=open(path,'r')
+    def openfile () :
+        path = filedialog.askopenfilename(filetype=(("txt","*.txt"),("All Files","*.*"))) #Open File to read having list of tracking ID
+        file = open(path,'r')
         return file
 
-    def readfile():
+    def readfile():         #Read Opened File
         for i in GettrackData.openfile():
             GettrackData.track.append(i.strip("\n"))
         return GettrackData.track
@@ -25,22 +25,22 @@ class GettrackData:
                 data=r.get(url)
             finally:
                 wfile = open("Tracking_Result.txt", 'a')
-                jdata= data.json()
+                jdata = data.json()
                 jdata.reverse()
                 for jin in jdata:
                     for i in jin:
                         if i in GettrackData.rdict:
                             GettrackData.rdict[i]=jin[i]
-                    print(GettrackData.rdict['dateWithNoSuffix']+" "+GettrackData.rdict['time']
+                            # uncomment this for detailed records
+                    """print(GettrackData.rdict['dateWithNoSuffix']+" "+GettrackData.rdict['time']
                           +" "+GettrackData.rdict['activityType']+" ("+GettrackData.rdict['origin']+") "+GettrackData.rdict['deliveryStatus'])
                     wfile.write("\n"+GettrackData.rdict['dateWithNoSuffix']+" "+GettrackData.rdict['time']
-                          +" "+GettrackData.rdict['activityType']+"("+GettrackData.rdict['origin']+") "+GettrackData.rdict['deliveryStatus'])
+                          +" "+GettrackData.rdict['activityType']+"("+GettrackData.rdict['origin']+") "+GettrackData.rdict['deliveryStatus'])"""
                 wfile.close()
 
 rcords=GettrackData.readfile()
 for i in range(len(rcords)):
     print("\n"+"Record for  " + GettrackData.track[i])
     GettrackData.getjsondata(i)
-    print("Latesdt updates are "+GettrackData.rdict['deliveryStatus']+" at "+GettrackData.rdict['origin']+"( "+GettrackData.rdict['activityType']+")")
-print("Your Tracking Details are stored in Tracking_Results.txt\nExiting Windows Please wait")
-time.sleep(15)
+    print("Latedt updates are "+GettrackData.rdict['deliveryStatus']+" at "+GettrackData.rdict['origin']+"( "+GettrackData.rdict['activityType']+")")
+#print("Your Tracking Details are stored in Tracking_Results.txt\nExiting Windows Please wait")
